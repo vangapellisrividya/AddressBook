932a39bc8c9f4e440b4679a0d6468162ec4c4b03
vangapellisrividyam <vangapellisreevidya@gmail.com>
2021-08-12 21:18:31 +0530

    my fourth commit

--- src/com/bl/addressBook/AddressBook.java
+++ src/com/bl/addressBook/AddressBook.java
@@ -1,69 +1,82 @@
 package com.bl.addressBook;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import com.bl.Addressbook.Person;
+
 public class AddressBook {
-	public class Person {
-		private String Firstname;
-		private String Lastname;
-		private String TempAddress;
-		private String street;
-		private String city;
-		private String state;
-		private String zip;
-		private String phonenumber;
-		private String email;
+	Scanner SC = new Scanner(System.in);
+	List<Person> addressBookList = new ArrayList<Person>();
+	Person person;
+	private String phonenumber;
+	private String street;
+	private String city;
+	private String state;
 
-		public String getFirstname() {
-			return Firstname;
+	public void add() {
+		person = new Person();
+		System.out.println(" Enter the Firstname");
+		String Firstname = SC.next();
+		person.setFirstname(Firstname);
+
+		System.out.println(" Enter the Lastname ");
+		String Lastname = SC.next();
+		person.setLastname(Lastname);
+
+		System.out.println(" Enter the temporary address");
+		String Tempaddress = SC.next();
+		person.setTempAddress(Tempaddress);
+
+		System.out.println(" Enter the street ");
+		String Street = SC.next();
+		person.setStreet(Street);
+
+		System.out.println(" Enter the city  ");
+		String City = SC.next();
+		person.setCity(City);
+
+		System.out.println(" Enter the state ");
+		String State = SC.next();
+		person.setState(State);
+
+		System.out.println(" Enter the zip ");
+		String Zip = SC.next();
+		person.setZip(Zip);
+
+		System.out.println(" Enter the phonenumber ");
+		String PhoneNumber = SC.next();
+		person.setPhoneNumber(PhoneNumber);
+
+		System.out.println(" Enter the email ");
+		String email = SC.next();
+		person.setEmail(email);
+
+		addressBookList.add(person);
+		System.out.println(addressBookList);
+
+	}
+
+	public void remove() {
+		System.out.println(" Enter the Firstname");
+		String Firstname = SC.next();
+		for (int i = 0; i < addressBookList.size(); i++) {
+			System.out.println(addressBookList.get(i));
+			if (addressBookList.get(i).getFirstname().equals(Firstname)) {
+				addressBookList.remove(i);
+				System.out.println(" successfully removed ");
+			}
+
 		}
+		System.out.println(addressBookList.size());
+		System.out.println(addressBookList);
 
-		public void setFirstname(String firstname) {
-			this.Firstname = firstname;
-		}
+	}
 
-		public String getTempAddress() {
-			return TempAddress;
-		}
-
-		public void setTempAddress(String TempAddress) {
-			this.TempAddress = TempAddress;
-		}
-
-		public String getLastname() {
-			return Lastname;
-		}
-
-		public void setLastname(String lastname) {
-			this.Lastname = lastname;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-
-		@Override
-		public String toString() {
-			return "InputUtilities [Firstname=" + Firstname + ", Lastname=" + Lastname + ", TempAddress=" + TempAddress
-					+ ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip + ", phonenumber="
-					+ phonenumber + ", email=" + email + "]\n";
-		}
-
-		public String getZip() {
-			return zip;
-		}
-
-		public void setZip(String zip) {
-			this.zip = zip;
-		}
 
 		public String getPhoneNumber() {
-			return phonenumber;
+			return getPhoneNumber();
 		}
 
 		public void setPhoneNumber(String PhoneNumber) {
@@ -71,7 +84,7 @@ public class AddressBook {
 		}
 
 		public String getStreet() {
-			return street;
+			return getStreet();
 		}
 
 		public void setStreet(String street) {
@@ -79,7 +92,7 @@ public class AddressBook {
 		}
 
 		public String getCity() {
-			return city;
+			return getCity();
 		}
 
 		public void setCity(String city) {
@@ -87,7 +100,7 @@ public class AddressBook {
 		}
 
 		public String getState() {
-			return state;
+			return getState();
 		}
 
 		public void setState(String state) {
@@ -96,4 +109,4 @@ public class AddressBook {
 
 	}
 
-}
+
